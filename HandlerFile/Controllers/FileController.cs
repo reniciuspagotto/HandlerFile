82ec1f6311b8c7233aa13985070e6a5c772c3c51
Renícius Pagotto Fostaini <renicius.pagotto@outlook.com>
2018-06-09 16:18:09 -0300

    Add project files.

---
+++ HandlerFile/Controllers/FileController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HandlerFile.Controllers
+{
+    [Route("api/file")]
+    public class FileController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPdfFile()
+        {
+            string filePath = System.IO.Directory.GetCurrentDirectory() + "\\File\\file1.jpg";
+
+            const string contentType = "application/jpg";
+            var result = new FileContentResult(System.IO.File.ReadAllBytes(filePath), contentType)
+            {
+                FileDownloadName = $"file1.jpg"
+            };
+
+            return result;
+        }
+    }
+}
---
+++ HandlerFile/Controllers/StatusController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HandlerFile.Controllers
+{
+    [Route("api/status")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult IsOnline()
+        {
+            return Ok(true);
+        }
+    }
+}
